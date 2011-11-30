5bc17e5cf674b8d32db491096f0865030cfbee72
Harper Reed <harper@nata2.org>
2011-11-29 23:29:23 -0600

    cleaned up appeal.js and made things a bit prettier

--- js/appeal.js
+++ js/appeal.js
@@ -1,24 +1,24 @@
 if (window == top) {
-	
-	$(document).ready(function() {
-      console.log('asd');
-		t = $('<div style="position: fixed; height: 144px; right: 200px; bottom: 0px;" id="programmer_holder"><a href="https://wikimediafoundation.org/wiki/Donate" target="_blank"><img border="0" style="display:none;" id="programmer" src="http://dopeman.org/brandon.png"   width="106" height="144"/></a></div>');
-		$('body').append(t);
+    bar = 6;
+    console.log('dirty');
+    t = $('<div style="position: fixed; height: 144px; right: 200px; bottom: 0px;" id="programmer_holder"><a href="http://gg.ly/t3xhIY" target="_blank"><img border="0" style="display:none;" id="programmer" src="https://harperreed.org/brandon.png"   width="106" height="144"/></a></div>');
+    $('body').append(t);
 
-		show = Math.floor(Math.random()*11);
-		console.log(show);
-		
-		if (show > 6){
-			timeout = (Math.floor(Math.random()*5)*1000);
-			if (timeout == 0) timeout = 4500;
-			console.log(timeout);
-			setTimeout(appeal,timeout);
-		}
-	});
+    show = Math.floor(Math.random()*11);
+    console.log("Brandon likelyhood for display (must be above "+bar+"): "+show);
+    
+    if (show > bar){
+        timeout = (Math.floor(Math.random()*5)*1000);
+        if (timeout == 0) timeout = 4500;
+        console.log("Brandon delay: " + timeout);
+        setTimeout(appeal,timeout);
+    }else{
+      console.log('sad day. no brandon');
+    }
 	
 	function appeal(){
 		pos = Math.floor((window.innerWidth-106)*Math.random()*1);
-		console.log(pos);
+		console.log("Brandon position: " + pos);
 		$('#programmer_holder').css('right',pos);
   		$('#programmer').show("slide", { direction: "down" }, 2000);
 	}
