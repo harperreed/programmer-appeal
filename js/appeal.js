2fcc3f319307f1b2d93b4bf78c1e6e23c1b3eba2
Harper Reed <harper@nata2.org>
2011-11-29 19:59:52 -0600

    initial run

---
+++ js/appeal.js
@@ -0,0 +1,28 @@
+
+
+		
+if (window == top) {
+	
+	$(document).ready(function() {
+		t = $('<div style="position: fixed; height: 144px; right: 200px; bottom: 0px;" id="programmer_holder"><img style="display:none;" id="programmer" src="http://dopeman.org/brandon.png"   width="106" height="144"/></div>');
+		$('body').append(t);
+
+		show = Math.floor(Math.random()*11);
+		console.log(show);
+		
+		if (show > 6){
+			timeout = (Math.floor(Math.random()*5)*1000);
+			if (timeout == 0) timeout = 4500;
+			console.log(timeout);
+			setTimeout(appeal,timeout);
+		}
+	});
+	
+	function appeal(){
+		pos = Math.floor((window.innerWidth-106)*Math.random()*1);
+		console.log(pos);
+		$('#programmer_holder').css('right',pos);
+  		$('#programmer').show("slide", { direction: "down" }, 2000);
+	}
+    
+}
