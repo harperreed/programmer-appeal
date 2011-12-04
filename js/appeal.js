7c30073066f6064a6de6f25309a75516c8ac6d0d
John Manoogian III <jm3@jm3.net>
2011-12-04 12:31:11 -0800

    remove debugging console.log statements so as not to interfere when debugging other pages

--- js/appeal.js
+++ js/appeal.js
@@ -1,24 +1,30 @@
+var debug = false;
+function dlog(s) {
+  if( debug )
+    console.log( s );
+}
+
 if (window == top) {
     bar = 7;
-    console.log('dirty');
+    dlog('dirty');
     t = $('<div style="position: fixed; height: 144px; right: 200px; bottom: 0px;" id="programmer_holder"><a href="http://gg.ly/t3xhIY" target="_blank"><img border="0" style="display:none;" id="programmer" src="https://harperreed.org/brandon.png"   width="106" height="144"/></a></div>');
     $('body').append(t);
 
     show = Math.floor(Math.random()*11);
-    console.log("Brandon likelyhood for display (must be above "+bar+"): "+show);
+    dlog("Brandon likelyhood for display (must be above "+bar+"): "+show);
     
     if (show > bar){
         timeout = (Math.floor(Math.random()*5)*1000);
         if (timeout == 0) timeout = 4500;
-        console.log("Brandon delay: " + timeout);
+        dlog("Brandon delay: " + timeout);
         setTimeout(appeal,timeout);
     }else{
-      console.log('sad day. no brandon');
+      dlog('sad day. no brandon');
     }
 	
 	function appeal(){
 		pos = Math.floor((window.innerWidth-106)*Math.random()*1);
-		console.log("Brandon position: " + pos);
+		dlog("Brandon position: " + pos);
 		$('#programmer_holder').css('right',pos);
   		$('#programmer').show("slide", { direction: "down" }, 2000);
 	}
